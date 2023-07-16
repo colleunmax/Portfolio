68b29c4f859b28da5dd39f6fcd2f009cfa8acce5
KoruJuriSan <mr.collejoulian@gmail.com>
2023-07-16 10:43:21 +0200

    replacing the audio iframe by the video not muted

--- src/routes/show-me/+page.svelte
+++ src/routes/show-me/+page.svelte
@@ -2,15 +2,8 @@
 	import rickRoll from "$lib/assets/rick.mp4"
 </script>
 
-<iframe
-	title="Never gonna give you up form rick ashley"
-	src={rickRoll}
-	allow="autoplay"
-	id="video"
-	style="display:none"
-/>
-<video src={rickRoll} autoplay controls muted />
-
+<!-- svelte-ignore a11y-media-has-caption -->
+<video src={rickRoll} autoplay controls />
 <style>
 	video {
 		pointer-events: none;
