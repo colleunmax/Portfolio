fefc42b48c24eae4026e9bc933512cd68d65c126
KoruJuriSan <mr.collejoulian@gmail.com>
2023-07-17 15:54:37 +0200

    Final boss fight

--- src/lib/components/hud.svelte
+++ src/lib/components/hud.svelte
@@ -8,7 +8,7 @@
 
 <div id="hud" class=" absolute top-0">
 	<h1 class=" text-4xl font-extrabold text-primary">{title}</h1>
-	<div class=" relative h-5 rounded w-56 bg-black overflow-hidden">
+	<div class=" relative h-5 rounded w-96 bg-black overflow-hidden">
         <div style={`width: ${pourcentage}%;`} class="block absolute h-5 bg-primary rounded"></div>
     </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
 	import { goto } from "$app/navigation"
 	import Hud from "$lib/components/hud.svelte"
+	import hit from "$lib/assets/hit.mp3"
+	import thisWay from "$lib/assets/thisWay.mp3"
 
+	function playHit() {
+		new Audio(hit).play()
+	}
+
+	function playBoss() {
+		const track = new Audio(thisWay)
+		track.volume = 0.1
+		track.loop = true
+		track.play()
+		return track
+	}
+
+	let cursorX: number;
+	let cursorY: number;
+
+	let musicBoss: Audio;
+	let movingInterval: NodeJS.Timeout;
+	let scenario = 0
+	let started = false
 	let showHud = false
+	let cinematic = false
+	let shielded = false
 	const firstName = "Colle"
 	const lastName = "Joulian"
 	let button = {
@@ -10,52 +33,120 @@
 		y: 0,
 		position: "relative",
 		pv: 100,
-		maxpv: 100
+		maxpv: 100,
+		cursor: "pointer"
 	}
-	function onFoo() {
-		console.log("mdr")
-	}
+	$: button.cursor = !cinematic && !shielded ? "pointer" : "not-allowed"
 </script>
 
 <div class=" flex flex-col gap-6">
-	<p class=" text-5xl font-bold selection:bg-transparent">
-		{#each firstName as letter}
-			<span class=" text-primary duration-[4.5s] hover:duration-100 hover:text-black">{letter}</span
-			>
-		{/each}
-		{#each lastName as letter}
-			<span class=" text-black duration-[4.5s] hover:duration-100 hover:text-primary">{letter}</span
-			>
-		{/each}
-	</p>
-	<p class=" text-xl">
-		I am capable of thing <a
-			class=" text-primary uppercase font-bold duration-200 hover:-translate-y-1 relative inline-block"
-			data-sveltekit-preload-data="hover"
-			href=".">you can't</a
-		>
-		do by your own ! <br />
-		<a
-			class=" text-primary uppercase font-bold duration-200 hover:-translate-y-1 relative inline-block"
-			data-sveltekit-preload-data="hover"
-			href=".">Want</a
-		> to see what ?
-	</p>
+	<div>
+		<p class=" text-5xl font-bold selection:bg-transparent">
+			{#each scenario >= 2 ? scenario === 3 ? "KILL" : "OUCH!" : firstName as letter}
+				<span class=" text-primary duration-[4.5s] hover:duration-100 hover:text-black"
+					>{letter}</span
+				>
+			{/each}
+			{#each scenario >= 2 ? scenario === 3 ? "YOU!" : "çaFAITmal!" : lastName as letter}
+				<span class=" text-black duration-[4.5s] hover:duration-100 hover:text-primary"
+					>{letter}</span
+				>
+			{/each}
+		</p>
+		{#if scenario === 1}
+			<p class="text-xl w-[600px]">
+				Excusez-moi, mais je sais que ça peut parraitre contre intuitif, mais pouvez vous arrêter de
+				me cliquer dessus ?
+			</p>
+		{:else if scenario == 2}
+			<p class="text-xl w-[600px]">
+				Petit fils de pute de merde mal élevé, tu me fait vraimment chier, alors tu veux a ce point me cliquer dessus ?
+				sache que je ne me laiserais pas faire !
+			</p>
+		{:else if scenario == 3}
+		<p class="text-xl w-[600px]">
+			ARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRG !!!!
+			I WILL KILL YOU AND ALL YOUR FAMILY !!!! DIEEEEEeeeee...
+		</p>
+		{:else}
+			<p class=" text-xl  w-[600px]">
+				I am capable of thing <a
+					class=" text-primary uppercase font-bold duration-200 hover:-translate-y-1 relative inline-block"
+					data-sveltekit-preload-data="hover"
+					href=".">you can't</a
+				>
+				do by your own !
+				<a
+					class=" text-primary uppercase font-bold duration-200 hover:-translate-y-1 relative inline-block"
+					data-sveltekit-preload-data="hover"
+					href=".">Want</a
+				> to see what ?
+			</p>
+		{/if}
+	</div>
 	<button
-		class=" hover:before:-right-[450%] hover:before:duration-[0.9s] hover:before:opacity-100 before:absolute before:block before:w-[400%] before:h-1/2 before:bg-white before:opacity-0 before:blur-lg before:-rotate-[15deg] before:right-full relative w-fit py-1 px-4 overflow-hidden bg-primary rounded-md text-2xl cursor-pointer text-white"
-		style={`transform: translate(${button.x}px, ${button.y}px); position: ${button.position}`}
-		on:mouseenter={async () => {
-			await new Promise((res) => setTimeout(res, 150))
-			button.x = Math.round(Math.random() * 500)
-			button.y = Math.round(Math.random() * 500)
-		}}
+		class=" hover:before:-right-[450%] hover:before:duration-[0.9s] hover:before:opacity-100 before:absolute before:block before:w-[400%] before:h-1/2 before:bg-white before:opacity-0 before:blur-lg before:-rotate-[15deg] before:right-full relative w-fit py-1 px-4 overflow-hidden bg-primary rounded-md text-2xl text-white"
+		style={`transform: translate(${button.x}px, ${button.y}px); position: ${button.position}; cursor: ${button.cursor}`}
 		on:click={() => {
-			if (!showHud) {
-				showHud = true
-			}
-			button.pv -= 10
-			if (button.pv === 0) {
-				goto("show-me")
+			if (!shielded && !cinematic) {
+				playHit()
+				if (!showHud) {
+					showHud = true
+				}
+				button.pv -= 2.5
+				if (scenario === 1) {
+					scenario = 2
+					cinematic = true
+					setTimeout(() => {
+						cinematic = false
+						musicBoss = playBoss()
+						movingInterval = setInterval(() => {
+							button.x = Math.random() * 300
+							button.y = Math.random() * 300
+						}, 800)
+					}, 4500)
+				}
+				if (scenario === 0) {
+					cinematic = true
+					scenario = 1
+					setTimeout(() => {
+						cinematic = false
+					}, 4500)
+				}
+				if (button.pv <= button.maxpv/2 && button.pv > button.maxpv/4 && scenario !== 3) {
+					clearInterval(movingInterval)
+					scenario = 3
+					cinematic = true
+					button.x = 0
+					button.y = 0
+						setTimeout(() => {
+							cinematic = false
+							movingInterval = setInterval(() => {
+							button.x = Math.random() * 300
+							button.y = Math.random() * 300
+						}, 500)
+					}, 3500)
+				}
+				if (button.pv <= button.maxpv/4 && button.pv > button.maxpv/8) {
+					clearInterval(movingInterval)
+						movingInterval = setInterval(() => {
+						button.x = Math.random() * 200
+						button.y = Math.random() * 200
+					}, 150)
+				}
+				if (button.pv <= button.maxpv/8) {
+					button.x = Math.random() * 450
+					button.y = Math.random() * 450
+					clearInterval(movingInterval)
+						movingInterval = setInterval(() => {
+						button.x = Math.random() * 450
+						button.y = Math.random() * 450
+					}, 400)
+				}
+				if (button.pv <= 0) {
+					musicBoss.pause()
+					goto("show-me")
+				}
 			}
 		}}>Show me!</button
 	>
