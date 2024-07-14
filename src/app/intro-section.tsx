233223d040d4458f599b609600f6cdb8629f620b
KoruJuriSan <mr.collejoulian@gmail.com>
2024-07-14 15:41:53 +0200

    ci: the subtitle will no longer be larger than the h1 in the intro section.

--- src/app/intro-section.tsx
+++ src/app/intro-section.tsx
@@ -1,9 +1,11 @@
 export default function Intro() {
     return <section id="introduction" className="flex flex-col items-center">
         <h1>Hey, I am-</h1>
-        <div>
+        <div className="inline-block">
             <h1 className="text-3xl">Colle Joulian</h1>
-            <h2 className="">I am a student in IT, future web developper</h2>
+            <div className="flex">
+                <h2 className="flex-grow w-0">I am a student in IT, future web developper</h2>
+            </div>
         </div>
     </section>
 }
