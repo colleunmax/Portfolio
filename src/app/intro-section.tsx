97d3bf119f18cd8a90a21f4157be1476eb12b7c0
KoruJuriSan <mr.collejoulian@gmail.com>
2024-07-15 19:41:43 +0200

    style: editing the intro section to match the design file

--- src/app/intro-section.tsx
+++ src/app/intro-section.tsx
@@ -1,10 +1,10 @@
 export default function Intro() {
     return <section id="introduction" className="flex flex-col items-center">
-        <h1>Hey, I am-</h1>
+        <h1 className="font-bold">Hey, I am-</h1>
         <div className="inline-block">
-            <h1 className="text-3xl">Colle Joulian</h1>
+            <h1 className="text-3xl font-bold">Colle Joulian</h1>
             <div className="flex">
-                <h2 className="flex-grow w-0">I am a student in IT, future web developper</h2>
+                <p className="flex-grow w-0">I am a student in IT, future web developper</p>
             </div>
         </div>
     </section>
