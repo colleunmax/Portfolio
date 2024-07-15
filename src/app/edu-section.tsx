ce42c0597e15ef9377770dfb2404215be93d2ade
KoruJuriSan <mr.collejoulian@gmail.com>
2024-07-15 19:40:59 +0200

    feat: adding the education section in the main page.tsx

---
+++ src/app/edu-section.tsx
@@ -0,0 +1,44 @@
+type cursus = {
+    name: string,
+    description: string,
+    date: string,
+    current?: boolean
+}
+
+const education: cursus[] = [
+    {
+        name: "CQ6 - Computer technician",
+        description: "Aumoniers du travail de Charleroi",
+        date: "2021-2023"
+    },
+    {
+        name: "Computer science Bachelor",
+        description: "Haute École en Hainaut",
+        date: "2023-2026",
+        current: true
+    }
+]
+
+export default function Education() {
+
+    const EductionCPs = education.map(({name, description, date, current}) => {
+
+        const currentSpan = current ? <span className="text-xs">(current)</span> : ""
+
+        return <li key={name} className="flex flex-col items-center italic">
+            <h2 className="inline">{name}</h2>
+            <p className="text-sm">{description}</p>
+            <div className="flex gap-2 items-center">
+                <span className="text-sm">{date}</span>
+                {currentSpan}
+            </div>
+        </li>
+    })
+
+    return <section id="education-section" className="flex flex-col items-center">
+        <h2 className="font-bold text-2xl mb-6">Education</h2>
+        <ul className="flex flex-col gap-6">
+            {EductionCPs}
+        </ul>
+    </section>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Education from "./edu-section"
 import Intro from "./intro-section"
 
 export default function Home() {
-    return <main>
+    return <main className="flex flex-col items-center gap-11 my-8">
         <Intro />
+        ----------------------------
+        <Education />
     </main>
 }
