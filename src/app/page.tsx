f20dbb743c7da80811dd1208d1e162a8284c0e2b
KoruJuriSan <mr.collejoulian@gmail.com>
2024-07-17 20:07:34 +0200

    ci: replacing old "---" separation by svg of the design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Separation from "@/components/home/separation"
 import Education from "./edu-section"
 import Intro from "./intro-section"
 import Projects from "./projects-section"
@@ -6,11 +7,11 @@ import Technologies from "./techno-section"
 export default function Home() {
     return <main className="flex flex-col items-center gap-11 my-8">
         <Intro />
-        ----------------------------
+        <Separation type={1} />
         <Education />
-        ----------------------------
+        <Separation type={2} />
         <Projects />
-        ----------------------------
+        <Separation type={3} />
         <Technologies />
     </main>
 }
