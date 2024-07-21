c028e3a4b0078b4b2e4d2e07d2cebeacbe433791
KoruJuriSan <mr.collejoulian@gmail.com>
2024-07-21 14:42:18 +0200

    ci: moving the color scheme to extend insteed of overwirting tailwindcss default one.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,17 +5,21 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    colors: {
-      "primary": "#599BD5",
-      "secondary": "#C1DFFA",
-      "ternary": "#E4F2FF",
-      "tern": "#607180",
-      "background": "#252C33",
-      "secondary-background": "#303437",
-      "scroll-bar": "#436179",
-      "love-gradiant-start": "#DB49FF",
-      "love-gradiant-end": "#FF3D00",
-      "transparent": "transparent"
+    extend: {
+      colors: {
+        "primary": "#599BD5",
+        "secondary": "#C1DFFA",
+        "ternary": "#E4F2FF",
+        "tern": "#607180",
+        "background": "#252C33",
+        "secondary-background": "#303437",
+        "scroll-bar": "#436179",
+        "love-gradiant-start": "#DB49FF",
+        "love-gradiant-end": "#FF3D00",
+      },
+      screens: {
+        "intermediate-md": { "min": "1400px"}
+      }
     }
   },
   plugins: [],
