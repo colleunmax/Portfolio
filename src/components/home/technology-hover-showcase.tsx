18c5532b695cda0cc411b88cb5ce3aa599e5b8fb
KoruJuriSan <mr.collejoulian@gmail.com>
2024-08-10 16:55:15 +0200

    ci: keyboard nav on techno section

--- src/components/home/technology-hover-showcase.tsx
+++ src/components/home/technology-hover-showcase.tsx
@@ -5,13 +5,28 @@ import { useCallback, useMemo, useState } from "react"
 export default function TechnologyHoverShowcase({Svg, name, confidence, preference}: techno) {
 
     const [isHovering, setHovering] = useState(false)
+    const [isFocus, setFocus] = useState(false)
 
     const handleMouseEnter = useCallback(() => {
-        setHovering(true)
-    }, [])
+        if (!isFocus) {
+            setHovering(true)
+        }
+    }, [isFocus])
 
     const handleMouseLeave = useCallback(() => {
+        if (!isFocus) {
+            setHovering(false)
+        }
+    }, [isFocus])
+
+    const handleFocusIn = useCallback(() => {
+        setHovering(true)
+        setFocus(true)
+    }, [])
+
+    const handleFocusOut = useCallback(() => {
         setHovering(false)
+        setFocus(false)
     }, [])
 
     const HoveringClassName = useMemo(() => {
@@ -20,7 +35,10 @@ export default function TechnologyHoverShowcase({Svg, name, confidence, preferen
 
     return <div 
     onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave} 
+    onMouseLeave={handleMouseLeave}
+    onFocus={handleFocusIn}
+    onBlur={handleFocusOut}
+    tabIndex={0}
     className="
     relative
     p-2
@@ -29,7 +47,10 @@ export default function TechnologyHoverShowcase({Svg, name, confidence, preferen
     before:absolute
     before:-z-10 before:left-1/2 before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2
     before:rounded-b-full
-    hover:before:block">
+    hover:before:block
+    focus:before:block
+    focus:outline-none
+    focus:outline-2 focus:outline-primary focus:rounded-full focus:-outline-offset-2">
         <Svg />
         <div className={` text-primary absolute top-0 -translate-y-full -translate-x-2 ${HoveringClassName}`}>
             <TechnologyShowcase name={name} Svg={Svg} confidence={confidence} preference={preference} />
