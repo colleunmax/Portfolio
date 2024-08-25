76f2457ed9c7de7a7323d47598800921ff0d1c6f
KoruJuriSan <mr.collejoulian@gmail.com>
2024-08-25 15:23:28 +0200

    ci: updating informations of the project section to reflect reality

--- src/app/projects-section.tsx
+++ src/app/projects-section.tsx
@@ -10,21 +10,15 @@ type project = {
 const projects: project[] = [
     {
         title: "Ching Chong Store",
-        description: "This website was created as part of my first-year web development course in a Bachelor's degree in Computer Science, with a focus on Telecommunications and Networks, at the Haute École en Hainaut.",
+        description: "This website was created as part of my first-year web development course for a Bachelor's degree in Computer Science. The project is a satiric website about China. The only constraint about this project was to only use HTML and CSS, no framwork is allowed.",
         imgPath: "/ching-chong-store.png",
-        alt: "Website with 'Le meilleur du made in China' written as the main title, showcasing various products, followed by the beginning of the presentation of a collection named Bing-Chilling."
+        alt: "Website with 'Historique Chinois' written as the main title, in the background an image of The Forbidden City, there is a description under the title: 'ous voulez connaître l’histoire de notre grande Chine, garantie 100% épique et époustouflant, voulez-vous apprendre de nouvelles choses à montrer à tous vos copains ?'"
     },
     {
         title: "sub.MASK",
-        description: "Notre équipe est composée de Joulian, Brendan et Romain. Ce projet a été réalisé dans le cadre de notre projet de fin d’études et le concept de celui-ci nous a été proposé par notre professeur de réseau Patrick Hainaut.",
+        description: "I created a website during my last year of high school for my teachers, with the objective of developing a fully functional basic network address calculator. Made with: React",
         imgPath: "/submask.png",
-        alt: "Deux grands titres respectivement 'Adresse Ipv4' et 'Masque de sous reseau', en dessous de chaque titre il y a 4 entrée pour écrire un nombre qui est séparé par un point, seul le masque de sous-réseau a un autre champ pour un seul nombre."
-    },
-    {
-        title: "Placeholder",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        imgPath: "/submask.png",
-        alt: "Lorem Ipsum"
+        alt: "Two main headings, IPv4 Address' and 'Subnet Mask,' each with four input fields below to enter a number separated by a dot. Only the subnet mask has an additional field for a single number."
     },
 ]
 
