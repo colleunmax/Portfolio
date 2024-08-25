22cddffc170ba147db60b8f131b6570cb7b8a50d
KoruJuriSan <mr.collejoulian@gmail.com>
2024-08-25 15:04:12 +0200

    fix: footer link outline is now visible on chromium based browsers

--- src/components/links/media-Link.tsx
+++ src/components/links/media-Link.tsx
@@ -8,9 +8,10 @@ type Props = {
 
 export default function MediaLink({ Svg, href, label }: Props) {
     return <a href={href} className="
-    text-tern 
-    hover:text-primary focus:text-primary
-    focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary" target="_black" aria-label={label}>
+    block
+    text-tern hover:text-primary focus:text-primary
+    focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary
+    " target="_black" aria-label={label}>
         <Svg />
     </a>
 }
